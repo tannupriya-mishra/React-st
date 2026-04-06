f49e39569d7910d223b4eb0aa244c772fd50e8c8
Tannu Priya <tanupriyamish@gmai.com>
2026-04-06 10:40:01 +0530

    Add first React component and project fies

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1>My First react App</h1>
+      <p>I am learning react today!</p>
+      
+    </div>
+  )
+}
+
+export default App
